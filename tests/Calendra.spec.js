654001f9bbe1e74aeb47a5027a2c31b1e1569020
matta445 <laluprasad678@gmail.com>
2026-06-06 23:50:24 +0100

    Updated Calendra code

---
+++ tests/Calendra.spec.js
@@ -0,0 +1,18 @@
+const{test,expect}=require('@playwright/test');
+
+test("Calendar Validations", async({page})=>
+{
+    const MonthNumber="6";
+    const Date="15";
+    const Year="2027";
+
+    await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+    await page.locator(".react-date-picker__inputGroup").click();
+    await page.locator(".react-calendar__navigation__label").click();
+    await page.locator(".react-calendar__navigation__label").click();
+    await page.getByText(Year).click();
+    await page.locator(".react-calendar__year-view__months__month").nth(Number(MonthNumber)-1).click();
+    await page.locator("//abbr[text()='"+Date+"']") .click();
+
+
+});
